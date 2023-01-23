a1a3ae223ed32f7ee3d0befac909cb725c821add
Gourav <gourav@g.com>
2023-01-23 09:30:24 +0530

    first commit

---
+++ src/basicprogram/FirstProgram.java
@@ -0,0 +1,32 @@
+package basicprogram;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is my first program"); 
+		
+		System.out.println("This is my second line");
+
+		System.out.println("This is my third line");
+
+		System.out.println("Last line");
+
+		System.out.println("8 + 2");
+
+		System.out.println(8 + 2);
+		
+		System.out.println(8 - 2);
+		
+		System.out.println(8 * 2);
+		
+		System.out.println(8 / 2);
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/datatypes/IntegralDataType.java
@@ -0,0 +1,30 @@
+package datatypes;
+
+public class IntegralDataType {
+	
+	public static void main(String[] args) {
+		
+//		datatype variablename = value;
+		
+		byte b = 100;
+		
+		byte b1 = 126;
+			
+		byte b2 = 127;
+		
+		byte b3 = -127;
+		
+		byte b4 = -128;	
+		
+		
+		System.out.println(b);
+		
+		System.out.println(b4);
+		
+		
+		
+		
+		
+	}
+
+}
