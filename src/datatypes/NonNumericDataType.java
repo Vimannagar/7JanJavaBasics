4efd9e9b62d8bbecf7371cbb94e5e8a947122892
Gourav <gourav@g.com>
2023-01-25 10:00:29 +0530

    Non primitive data type

--- src/datatypes/NonNumericDataType.java
+++ src/datatypes/NonNumericDataType.java
@@ -21,6 +21,21 @@ public class NonNumericDataType {
 		
 		System.out.println(variable);
 		
+//		boolean data type
+		
+		boolean b = false;
+		
+		boolean g1 = false;
+		
+		
+		boolean c1 = false;
+		
+		
+		System.out.println(g1);
+		
+		
+		
+		
 		
 	}
 
---
+++ src/datatypes/NonPrimitiveDataType.java
@@ -0,0 +1,26 @@
+package datatypes;
+
+public class NonPrimitiveDataType {
+	
+	public static void main(String[] args) {
+		
+		String  s1 = "Velocity";
+		
+		String s2 = "Corporate";
+		
+//		concatination
+		System.out.println(s1+s2);//VelocityCorporate
+		
+		System.out.println(s1+"     "+s2);//Velocity     Corporate
+		
+		System.out.println(s1+' '+2);//Velocity 2
+		
+		System.out.println(s1+2+3);//Velocity23
+		
+		System.out.println(2+3+s2);//5Corporate
+		
+		
+		
+	}
+
+}
