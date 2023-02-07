0cf337b5b6025523ebaff5f875938de2a8e1a1d7
Gourav <gourav@g.com>
2023-02-07 10:55:09 +0530

    local variable

---
+++ src/variables/LocalVariable.java
@@ -0,0 +1,54 @@
+package variables;
+
+public class LocalVariable {
+	
+static int i = 70;
+
+int j = 60;
+
+
+	
+		public static void main(String[] args) {
+//		Local variable 
+//		syntax: datatype variable_name = value
+		
+		int i = 50;
+		
+		String s = "hello";
+		
+		char c ='v';
+		
+		System.out.println(c);
+		
+		
+		System.out.println(i);
+		
+	
+	}
+	
+	public static void m1()
+	{
+		int j = 20;
+		
+		int i = 90;
+		
+		System.out.println(i);
+		System.out.println(j);
+	}	
+		
+		
+		
+	
+	
+	public void m2()
+	{
+		int k = 30;
+		
+		System.out.println(k);
+	}
+	
+	
+	
+	
+	
+}
