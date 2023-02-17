a0a1089346215a8b02566704eb85421c67625850
Gourav <gourav@g.com>
2023-02-17 10:12:31 +0530

    Loops discussion

--- src/loops/Test.java
+++ src/loops/Test.java
@@ -1,5 +1,19 @@
 package loops;
 
 public class Test {
+	
+	
+	public static void main(String[] args) {
+		
+		
+//		WAP to print first 1000 numbers which can get completely divide by 4--> While loop
+		
+		
+//		WAP to print the numbers which get divide by 2 until 100 ---> for loop
+	
+//	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+	
+//		count =1+1 + 1
+	}
 
 }
