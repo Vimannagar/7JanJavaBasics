03c65c9a2b56a77b0438b68ab31e6f98662a597f
Gourav <gourav@g.com>
2023-02-18 10:09:30 +0530

    star patterns

--- src/loops/Test.java
+++ src/loops/Test.java
@@ -8,12 +8,38 @@ public class Test {
 		
 //		WAP to print first 1000 numbers which can get completely divide by 4--> While loop
 		
+		int count =0;
+		
+		int number = 1;
+		
+		while(count <=1000)
+		{
+			if(number%4==0)
+			{
+				System.out.println(number);
+				count++;
+			}
+			
+			number++;
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 //		WAP to print the numbers which get divide by 2 until 100 ---> for loop
 	
 //	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
 	
 //		count =1+1 + 1
-	}
 
-}
+
+
+}}
