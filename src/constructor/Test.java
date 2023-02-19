b6a634cf2c78a41514e428b0f65325af2fe4f7f8
Gourav <gourav@g.com>
2023-02-19 10:04:38 +0530

    constructor

---
+++ src/constructor/Test.java
@@ -0,0 +1,44 @@
+package constructor;
+
+public class Test {
+	
+	
+	
+//	syntax of constructor-
+	
+//	access_modifier classname()
+//	{
+//		actions to be performed
+//	}
+	
+	
+	
+	public Test()
+	{
+		System.out.println("Hello I am constructor");
+	}
+	
+	public Test(int i)
+	{
+		System.out.println("Hello I am constructor");
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		
+		Test t1 = new Test();
+		
+		
+	
+		
+		
+		
+		
+	
+		
+	}
+
+}
